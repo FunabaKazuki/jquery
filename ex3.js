da54cadca883ee2796f9d9dac8fc4a97e0641f04
funabakazuki <kazuki.funaba@rakus.partners.co.jp>
2021-05-17 11:50:15 +0900

    ex1,ex2

--- ex3.js
+++ ex3.js
@@ -1,5 +1,24 @@
- 'use strict';
- $(function(){
- alert("HELLO WORLD"); //ポップアップで表⽰
- console.log("hello world"); //コンソールに表⽰
- });
+"use strict";
+$(function () {
+  alert("HELLO WORLD"); //ポップアップで表⽰
+  console.log("hello world"); //コンソールに表⽰
+
+  $("#show").on("click", function () {
+    $("#nameshow").text("船場");
+  });
+  $("#show").on("click", function () {
+    let targetText = document.getElementById('nameshow');
+    if(targetText.style.color=='blue'){
+        $("#nameshow").css("color","red");
+    }else{
+    $("#nameshow").css("color","blue");
+    }
+  });
+//  let targetText = document.getElementById('nameshow');
+ // if(targetText.style.color=='blue'){
+ //   $("#show").on("click", function () {
+ //      $("#nameshow").css("color","red");
+ //     }); 
+ // }
+
+});
